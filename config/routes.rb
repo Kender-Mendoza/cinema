3a533d2f3b53064ff4cce52a4071f246a1266568
kender-mendoza <kendermendoza591@outlook.com>
2020-07-25 15:27:53 -0600

    Se cambio la ruta de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :movies
   resources :cines
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cines#index'
 end
