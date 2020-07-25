0fba6fc02400bb75031bf54775c8c60e97185fc9
kender-mendoza <kendermendoza591@outlook.com>
2020-07-25 13:23:11 -0600

    Se ejecuto la migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_234456) do
+ActiveRecord::Schema.define(version: 2020_07_25_025134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,15 @@ ActiveRecord::Schema.define(version: 2020_07_24_234456) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.time "duration"
+    t.datetime "date_time_movie"
+    t.bigint "cine_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cine_id"], name: "index_movies_on_cine_id"
+  end
+
+  add_foreign_key "movies", "cines"
 end
